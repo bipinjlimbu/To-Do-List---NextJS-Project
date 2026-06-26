4f6743b2781d8371ed109f2608e56b79500db123
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-26 19:22:33 +0545

    Get and Add Todo Services Added

---
+++ my-app/services/ToDoServices.ts
@@ -0,0 +1,21 @@
+import { db } from "@/db/index";
+
+export async function getTodos() {
+    try {
+        const data = await db.query('SELECT * FROM todos');
+        return data;
+    } catch (error) {
+        console.error('Error fetching todos:', error);
+        throw error;
+    }
+}
+
+export async function addTodo(title: string) {
+    try {
+        const data = await db.query('INSERT INTO todos (title, completed) VALUES (?, ?)', [title, false]);
+        return data;
+    } catch (error) {
+        console.error('Error adding todo:', error);
+        throw error;
+    }
+}
